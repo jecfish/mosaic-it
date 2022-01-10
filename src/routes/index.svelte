e95ba23ea6e42e91d0c08ca1145e752e09253873
Paul Kinlan <paul.kinlan@gmail.com>
2022-01-10 22:59:15 +0000

    Adding file-drop element to the project.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,65 @@
-<script context="module">
-  import './node_module/file-drop';
-</script>
+<svelte:head>
+  <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p><file-drop></file-drop></p>
+    div#svelte {
+      height:100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  </style>
+</svelte:head>
+
+<script>
+  import 'file-drop-element';
+  import { onMount } from 'svelte';
+
+  let dropTarget;
+  let droppedFile;
+  let fileUrl;
+
+  onMount(() => {
+    dropTarget.addEventListener('filedrop', (fileDropEvent) => {
+      let file = fileDropEvent.files[0];
+      if (fileUrl != undefined) {
+        URL.revokeObjectURL(fileUrl);
+      }
+      fileUrl = URL.createObjectURL(file);
+    });
+  });
+</script>
+<style>
+  file-drop {
+    height: 80vh;
+    width: 80vw;
+    border-radius: 10px;
+    box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+    
+  :global(file-drop.drop-valid) {
+    background-color: green;
+  }
+  
+  :global(file-drop.drop-invalid) {
+    background-color: red;
+  }
+</style>
+
+<h1>Welcome to Auto Mosaic.</h1>
+<file-drop bind:this={dropTarget} accept="image/*">
+{#if fileUrl == undefined}
+  <p>Drop an image file here to auto-mosasic.</p>
+{:else}
+    <img bind:this={droppedFile} src={fileUrl}>
+{/if}
+</file-drop>
